1bf6ae1f84297ab1e20529ab8d942986d18731ef
Rve27 <rve27github@gmail.com>
2026-04-26 05:27:52 +0700

    components: AppBars: Animate title padding and back button in MediumTopAppBar

--- app/src/main/java/com/rve/systemmonitor/ui/components/AppBars.kt
+++ app/src/main/java/com/rve/systemmonitor/ui/components/AppBars.kt
@@ -2,10 +2,16 @@
 
 package com.rve.systemmonitor.ui.components
 
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.FilledTonalIconButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MediumTopAppBar
@@ -19,9 +25,11 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.material3.rememberTooltipState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.composables.icons.materialsymbols.roundedfilled.R.drawable.materialsymbols_ic_settings_rounded_filled
 
 object AppBars {
@@ -75,10 +83,16 @@ object AppBars {
 fun ExitUntilCollapsedMediumTopAppBar(title: String, onNavigateBack: () -> Unit, scrollBehavior: TopAppBarScrollBehavior) {
     MediumTopAppBar(
         title = {
+            val titleStartPadding = animateDpAsState(
+                targetValue = if (scrollBehavior.state.collapsedFraction > 0.5f) 6.dp else 0.dp,
+                animationSpec = tween( 250),
+                label = "titleStartPadding",
+            )
             Text(
                 text = title,
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
+                modifier = Modifier.padding(start = titleStartPadding.value),
             )
         },
         navigationIcon = {
@@ -93,13 +107,29 @@ fun ExitUntilCollapsedMediumTopAppBar(title: String, onNavigateBack: () -> Unit,
                 },
                 state = rememberTooltipState(),
             ) {
-                IconButton(
-                    onClick = onNavigateBack,
-                ) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
-                        contentDescription = "Back",
-                    )
+                Crossfade(
+                    targetState = scrollBehavior.state.collapsedFraction > 0.5f,
+                    animationSpec = tween(500)
+                ) { scrolled ->
+                    if (scrolled) {
+                        FilledIconButton(
+                            onClick = onNavigateBack,
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                                contentDescription = "Back",
+                            )
+                        }
+                    } else {
+                        FilledTonalIconButton(
+                            onClick = onNavigateBack,
+                        ) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                                contentDescription = "Back",
+                            )
+                        }
+                    }
                 }
             }
         },
