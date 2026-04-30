61fac25d3c83c4744b7f2d20e10515af682bfa9b
Rve27 <rve27github@gmail.com>
2026-04-30 21:48:58 +0700

    MainActivity: Keep screen on in debug build
    Signed-off-by: Rve27 <rve27github@gmail.com>

--- app/src/main/java/com/rve/systemmonitor/MainActivity.kt
+++ app/src/main/java/com/rve/systemmonitor/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.rve.systemmonitor
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -29,6 +30,10 @@ class MainActivity : ComponentActivity() {
 
         splashScreen.setKeepOnScreenCondition {
             viewModel.uiState.value is MainUiState.Loading
+        }
+
+        if (BuildConfig.DEBUG) {
+            window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         }
 
         setContent {
